f8fb6b28aa657ea5c9e84b7010e58fe7a4728cd4
alexngUNC <ageorgiev@unc.edu>
2022-08-03 12:54:05 -0400

    working with strings

---
+++ strings.rb
@@ -0,0 +1,14 @@
+puts "Hello, world."
+
+# Basic printing with variables
+person = "Stan"
+
+puts ("Hello,\n" + person)
+
+shout = "    hellooooo     "
+
+puts shout.upcase().strip()
+
+puts shout.length
+
+puts shout.include? "o"
